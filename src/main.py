0c47f96a33544b55b0e655cd46db88e4fb9769e2
WictorHiago <wictor.hsc@gmail.com>
2023-11-27 02:53:04 -0400

    feat: create functions 'create_speech, create_transcript, whisper,chat_completions'

---
+++ src/main.py
@@ -0,0 +1,81 @@
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+
+load_dotenv()
+openai_key = os.getenv("OPENAI_KEY")
+from openai import OpenAI
+
+client = OpenAI(api_key=openai_key)
+
+def chat_completion(message:str):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        max_tokens=300,
+        temperature=0.3,
+        top_p=1,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message},
+        ]
+    )
+
+    return response.choices[0].message.content
+
+# ---RUN TEST chat_completion---
+# gpt_response = chat_completion("how to lean python fast?")
+# print(gpt_response)
+# ----------------------------------------------
+
+def create_speech():
+    speech_file_path = Path(__file__).parent / "new-audio.mp3"
+    response = client.audio.speech.create(
+        model="tts-1",
+        input="Ahhh a.. Zeeh da Manhgha",
+        voice="onyx",
+        response_format="mp3",
+        speed=1.0
+    )
+
+    response.stream_to_file(speech_file_path)
+
+# ---RUN TEST create_speech---
+# create_speech()
+# ----------------------------------------------
+
+# TRANSCRIBE AUDIO TO LANGUAGES ru|en|fr|es|de|it ...
+def create_transcript():
+    audio_file = open("audio.mp3", "rb")
+    transcript = client.audio.transcriptions.create(
+    model="whisper-1",
+    file=audio_file,
+    language="fr",
+    response_format="text",
+    )
+
+    return transcript
+
+# ---RUN TEST create_transcript---
+audio_transcript = create_transcript()
+print(audio_transcript)
+# ----------------------------------------------
+
+# TRANSLATE AUDIO TO ENGLISH
+def whisper():
+    audio_file = open("audio.mp3", "rb")
+    transcript = client.audio.translations.create(
+    model="whisper-1", 
+    file=audio_file,
+    response_format="text",
+    )
+
+    return transcript
+    
+
+# ---RUN TEST whisper---
+# whisper_response = whisper()
+# print(whisper_response)
+# ----------------------------------------------
+
+
